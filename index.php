940465827a8528dbcec77473e691e3f64c7e7794
asimon <asimon@I1-10-1-1-8>
2019-01-28 11:59:12 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Este es el primer echo <br/>';
+        
+        echo 'voy a hacer el primer commit en local';
+        
+        ?>
+    </body>
+</html>
